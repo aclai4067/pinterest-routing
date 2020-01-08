37e922f3745f8f15e0a20840a631a0b0551a7ea2
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-07 19:51:03 -0600

    read complete on boards

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,16 +1,28 @@
 import './Home.scss';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import boardData from '../../../helpers/data/boardData';
+import authData from '../../../helpers/data/authData';
+import Board from '../../shared/Board/Board';
 
 class Home extends React.Component {
-  render() {
-    const boardId = '12345';
+  state = {
+    boards: [],
+  }
 
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => {
+        this.setState({ boards });
+      }).catch((err) => console.error(err));
+  }
+
+  render() {
     return (
       <div className='Home'>
-        <h1>Home Page</h1>
-        <Link className='btn btn-dark' to='/board/new'>Create New Board</Link>
-        <Link className='btn btn-light' to={`/board/${boardId}`}>Visit Board {boardId}</Link>
+        <h1 className='m-4'>Home Page</h1>
+        <div className='boards d-flex flex-wrap justify-content-around'>
+          {this.state.boards.map((board) => <Board key={board.id} board={board} />)}
+        </div>
       </div>
     );
   }
