6042b47b4d5ee400536a8a9f1d5a19df855f20b6
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-07 20:33:08 -0600

    pins print

--- src/components/pages/SingleBoard/SingleBoard.js
+++ src/components/pages/SingleBoard/SingleBoard.js
@@ -1,13 +1,40 @@
 import './SingleBoard.scss';
 import React from 'react';
+import boardData from '../../../helpers/data/boardData';
+import pinData from '../../../helpers/data/pinData';
+import Pin from '../../shared/Pin/Pin';
 
 class SingleBoard extends React.Component {
-  render() {
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  getPinData = (boardId) => {
+    pinData.getPinsByBoardId(boardId)
+      .then((pins) => {
+        this.setState({ pins });
+      }).catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
     const { boardId } = this.props.match.params;
+    boardData.getSingleBoard(boardId)
+      .then((board) => {
+        this.setState({ board: board.data });
+        this.getPinData(boardId);
+      }).catch((err) => console.error(err));
+  }
+
+  render() {
+    const { board } = this.state;
     return (
       <div className='SingleBoard'>
-        <h1>Single Board Page</h1>
-        <h2>Current Board Id is {boardId}</h2>
+        <h1>{board.name}</h1>
+        <h2>{board.description}</h2>
+        <div className='d-flex flex-wrap justify-content-around'>
+          {this.state.pins.map((pin) => <Pin key={pin.id} pin={pin} />)}
+        </div>
       </div>
     );
   }
