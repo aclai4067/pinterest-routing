bb33a83c2996df6642c9d1d8d70bcf164d0064b9
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-07 19:18:21 -0600

    fixed navbar formatting

--- src/components/shared/MyNav/MyNav.js
+++ src/components/shared/MyNav/MyNav.js
@@ -1,5 +1,6 @@
 import './MyNav.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import PropTypes from 'prop-types';
@@ -15,21 +16,35 @@ class MyNav extends React.Component {
   }
 
   render() {
-    const { authed } = this.props;
+    const buildNavbar = () => {
+      const { authed } = this.props;
+      if (authed) {
+        return (
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/'>Boards</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/board/new'>New Board</Link>
+            </li>
+            <li className='nav-item'>
+              <button className="logout btn btn-warning my-2 my-sm-0" onClick={this.logoutEvent}>Log Out</button>
+            </li>
+          </ul>
+        );
+      }
+      return (<ul className='navbar-nav ml-auto'>></ul>);
+    };
 
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Pinterest</a>
+        <Link className="navbar-brand" to="/">Pinterest</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
           aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            {authed && (<button className="logout btn btn-warning my-2 my-sm-0" onClick={this.logoutEvent}>Log Out</button>)}
-          </form>
+          { buildNavbar() }
         </div>
       </nav>
     );
