5bb42f34ccf95b7d93f70787884e71763723ac67
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-07 21:17:02 -0600

    delete board works

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -9,10 +9,21 @@ class Home extends React.Component {
     boards: [],
   }
 
-  componentDidMount() {
+  getBoards = () => {
     boardData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
+      }).catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getBoards();
+  }
+
+  deleteBoard = (boardId) => {
+    boardData.discardBoard(boardId)
+      .then(() => {
+        this.getBoards();
       }).catch((err) => console.error(err));
   }
 
@@ -21,7 +32,7 @@ class Home extends React.Component {
       <div className='Home'>
         <h1 className='m-4'>Home Page</h1>
         <div className='boards d-flex flex-wrap justify-content-around'>
-          {this.state.boards.map((board) => <Board key={board.id} board={board} />)}
+          {this.state.boards.map((board) => <Board key={board.id} board={board} deleteBoard={this.deleteBoard} />)}
         </div>
       </div>
     );
